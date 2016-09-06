8ff47e4b6e0fd76617b46239e981e5531869bf8a
Praakrit Pradhan <pradhapt@mail.uc.edu>
2016-09-06 19:59:28 -0400

    %% Adding comments %%

--- script.m
+++ script.m
@@ -12,23 +12,30 @@ for x = 1:150
         iris(x, 5) = 3.0;
     end
 end
+%%  
 
-% added a comment
+%k = input('Enter k value: ');
 
 k = 3;
 
+%%
+
+% Normalizing data set
 irisWork(:,1) = irisWork(:,1) - min(iris(:,1)) + 1;
 irisWork(:,2) = irisWork(:,2) - min(iris(:,2)) + 1;
 irisWork(:,3) = irisWork(:,3) - min(iris(:,3)) + 1;
 irisWork(:,4) = irisWork(:,4) - min(iris(:,4)) + 1;
 
+% Finding range for each attribute
 plRange = max(irisWork(:,1)) + 1;
 pwRange = max(irisWork(:,2)) + 1;
 slRange = max(irisWork(:,3)) + 1;
 swRange = max(irisWork(:,4)) + 1;
 
+% Descritized set allocation
 descSet = irisWork;
 
+% descritizing the data
 descSet(:,1) = descSet(:,1)./(plRange/k);
 descSet(:,2) = descSet(:,2)./(pwRange/k);
 descSet(:,3) = descSet(:,3)./(slRange/k);
@@ -36,6 +43,7 @@ descSet(:,4) = descSet(:,4)./(swRange/k);
 
 descSet = ceil(descSet);
 
+% assigning the descritized set into flowers 
 sentosa = descSet(1:50,:);
 verisicolor = descSet(51:100,:);
 virginica = descSet(101:150,:);
