30673ce93a5827cb8129b7d97b6d524a33b6391f
Praakrit Pradhan <pradhapt@mail.uc.edu>
2016-09-08 14:35:46 -0400

    minor change.
    There was a small error in test. Making it so that Sentosa would never
    get a 100% on the bin count total.
    - Fixed

--- test.m
+++ test.m
@@ -1,13 +1,20 @@
-%% test function
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Machine Learning %
+% Assignment 1 %
+% Team: Buckey James, Li Kendrick, Pradhan Praakrit %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%test function
 %  k = number of bins
 %  returns [sentosaList, verisicolorList, verginicaList, sentosaTest,
 %  verisicolorTest, verginicaTest]
+
 function [results] = test(k)
     [sentosaList, verisicolorList, virginicaList, sentosaTest, verisicolorTest, virginicaTest] = train(k);
     
-    results = zeros(24, 3);
+    results = zeros(25, 3);
     % sentosa testing
-    for t = 1:24 % length of test list
+    for t = 1:25 % length of test list
         for a = 1:4  % number of attributes
             results(t,1) = any(sentosaTest(t,a) == sentosaList{a});
         end
