87fbb30fb24095195687bd10e00e8dab85453d6e
likt <likt@mail.uc.edu>
2016-09-18 16:21:07 -0400

    Updated Code
    ID3 is now at the point where we have the Gain of all the columns, now
    is the tree part which I have no idea how to do.

--- homework 2/Homework2_rough3.m
+++ homework 2/Homework2_rough3.m
@@ -67,3 +67,28 @@ pC2 = [binC2(:,1)/a(1,2), binC2(:,2)/a(2,2), binC2(:,3)/a(3,2)];
 pC3 = [binC3(:,1)/a(1,3), binC3(:,2)/a(2,3), binC3(:,3)/a(3,3)];
 pC4 = [binC4(:,1)/a(1,4), binC4(:,2)/a(2,4), binC4(:,3)/a(3,4)];
 
+G = zeros(1,4);
+H = zeros(1,4);
+
+Hc1 = -pC1.*log2(pC1);
+Hc1(find(isnan(Hc1))) = 0;
+H(1) = sum(sum(Hc1, 1).*p(1,:));
+G(1) = hT - H(1);
+
+Hc2 = -pC2.*log2(pC2);
+Hc2(find(isnan(Hc2))) = 0;
+H(2) = sum(sum(Hc2, 1).*p(2,:));
+G(2) = hT - H(2);
+
+Hc3 = -pC3.*log2(pC3);
+Hc3(find(isnan(Hc3))) = 0;
+H(3) = sum(sum(Hc3, 1).*p(3,:));
+G(3) = hT - H(3);
+
+Hc4 = -pC4.*log2(pC4);
+Hc4(find(isnan(Hc4))) = 0;
+H(4) = sum(sum(Hc4, 1).*p(4,:));
+G(4) = hT - H(4);
+
+H
+G
