31cc73ddf4b8a0f2a39c11e4e9cf8c71c46a7fb3
likt <likt@mail.uc.edu>
2016-11-05 20:19:02 -0400

    IN THE PROCESS OF REDOING ASSIGNMENT

---
+++ homework 3/classify.m
@@ -0,0 +1,18 @@
+%% classify
+%%
+function [classification, SVMvalues] = classify(dataset, test, alpha, b)
+    classifier = cell2mat(dataset(:, 2));
+    data = cell2mat(dataset(:, 1));
+    testData = cell2mat(test(:, 1));
+    numData = length(testData);
+    
+    SVMvalues = zeros(numData, 1);
+    classification = zeros(numData, 1);
+    
+    for i = 1:numData
+        SVMvalues(i) = sum((data*testData(i,:)').*classifier.*alpha) + b - classifier(i);
+    end
+    
+    classification(SVMvalues >= 0) = 1;
+    classification(SVMvalues < 0) = -1;
+end
--- homework 3/homeworkDoc.m
+++ homework 3/homeworkDoc.m
@@ -1,20 +1,20 @@
-% test doc
-
 % clean workspace
 clear all; clc;
 
-heart = dlmread('heart.txt');
+heart = dlmread('heart2.txt');
 median = heart(1,:);        % Get the median : the first row of data
 heart(1,:) = [];            % Remove the first row of data
 hLength = length(heart);    % Calculate length of the data
 colNum = length(heart(1,:));% Calculate num of columns
-b = 0;                      % bias is initially 0
-epsilon = 1.0e-12;          % define epsilon, we don't know where this comes from
+
+max_passes = 5;             % max # of times to iterate over ?’s without changing
+C = 10;                     % define C, see http://stats.stackexchange.com/a/159051
+tolerance = 1.0e-12;        % define tolerance, we don't know where this comes from but should be small
 
 % Set the number of tests from the training set
 teLength = 50;                  % testing length
 if teLength > hLength
-    teLength = 50;
+    teLength = 2;               % we need some training data
 end
 trLength = hLength - teLength;  % training length
 
@@ -31,49 +31,35 @@ for n = 1:hLength
     end
 end
 
-% gen initial alpha and vals array
-%alpha = zeros(trLength, 1);
-alpha = ones(trLength, 1); % test alpha
+% START LEARNING
+%------------------------------------------------------
+
+% gen initial values
+alpha = zeros(trLength, 1);
 vals = zeros(trLength, 1);
+b = 0;
 
 correct = zeros(trLength, 1);
 %while sum(correct) < trLength
-num = 1;                    % test code so that this only runs once
-while num == 1              % test code so that this only runs once
-    num = num - 1;          % test code so that this only runs once
+num = 500;                                             % test code
+while num > 0 && sum(correct) < trLength              % test code
+    num = num - 1;                               % test code
     
     % calculate weight vector
-    weightV = (alpha.*cell2mat(S(:,2)))'*cell2mat(S(:,1));
-    weightV = weightV';
+    weightV = ((alpha.*cell2mat(S(:,2)))'*cell2mat(S(:,1)))';
     
-    % calculate KKT    
+    % calculate KKT
     KKTV = ((cell2mat(S(:,1))*weightV + b).*cell2mat(S(:,2)) - 1).*alpha;
     
     % pick x1
     [~, i1] = max(KKTV);
     x1 = S{i1, 1};
     
-    %{
-    % calculate abs(small e) vector WE DONT USE THIS
-    eV = zeros(trLength, 1);
-    for i = 1:trLength
-        eV(i) = abs(E(1, S, alpha, b) - E(i, S, alpha, b));
-    end
-    
-    % pick x1
-    [~, i2] = max(eV);
-    x2 = S{i2, 1};
-    %}
-    
     % calculate abs(small e) vector
-    %eV = zeros(trLength, 1);
     EV = zeros(trLength, 1);
     for i = 1:trLength
-        %eV(i) = smallE(i, S, alpha, i1);
-        %eV(i) = E(i1, S, alpha, b) - E(i, S, alpha, b);
         EV(i) = sum((cell2mat(S(:,1))*S{i,1}').*cell2mat(S(:,2)).*alpha) + b - S{i,2};
-    end
-    
+    end    
     eV = EV(i1) - EV;
     
     % pick x2
@@ -83,43 +69,35 @@ while num == 1              % test code so that this only runs once
     % calculate k
     k = kernel(x1, x1) + kernel(x2, x2) - 2*kernel(x1, x2);
     
-    % update alpha 2
-    oldAlpha2 = alpha(i2);
-    alpha(i2) = oldAlpha2 + (S{i2, 2}*eV(i2))/k;
-    
-    % update alpha 1
-    alpha(i1) = alpha(i1) + S{i1, 2}*S{i2, 2}*(oldAlpha2 - alpha(i2));
-    
-    % simplify 
-    alpha(alpha < epsilon) = 0;
+    if k ~= 0
+        % update alpha 2
+        oldAlpha2 = alpha(i2);
+        alpha(i2) = oldAlpha2 - (S{i2, 2}*eV(i2))/k;
+
+        % update alpha 1
+        alpha(i1) = alpha(i1) + S{i1, 2}*S{i2, 2}*(oldAlpha2 - alpha(i2));
+
+        % simplify 
+        alpha(alpha < tolerance) = 0;
+    end
     
     % calculate new b
     alphaGT0 = find(alpha > 0);
     ialphaGT0 = alphaGT0(ceil(rand*length(alphaGT0)));
-    b = 1/S{ialphaGT0, 2} - dot(weightV, S{ialphaGT0, 1});
+    b = 1/S{ialphaGT0, 2} - kernel(weightV, S{ialphaGT0, 1});
+    %b = (KKTV(ialphaGT0)/alpha(ialphaGT0) + 1)/S{ialphaGT0, 2} - kernel(weightV, S{ialphaGT0, 1});
     
     % test classification
-    SVMValues = zeros(trLength, 1);
-    prediction = zeros(trLength, 1);
-    for i = 1:trLength
-        SVMValues(i) = E(i, S, alpha, b);
-    end
+    [prediction SVMValues] = classify(S, S, alpha, b);
     
-    prediction(SVMValues < 0) = -1;
-    prediction(SVMValues >= 0) = 1;
-    
-    correct = prediction == cell2mat(S(:,2));
+    correct = prediction == cell2mat(S(:, 2));
 end
 
 %{
-directly from assignment
-8. Select ?i > 0, calculate b (from KKT conditions)
-9. Test for classification;
-10. Repeat from Step 2. until classified.
-%}
-
-%{
 references:
+http://cs229.stanford.edu/materials/smo.pdf <------ USE THIS, MAKES WAY
+MORE SENSE
+
 SVM_KernelsOctober11-2016.pdf
 SVM_Notes10-03-16.pdf
 https://en.wikipedia.org/wiki/Sequential_minimal_optimization
